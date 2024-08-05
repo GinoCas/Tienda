37402006ad51bbe23702796715e0bc52d9d0d7ee
GinoCas <gino.casentini.5@gmail.com>
2024-08-04 22:52:41 -0300

    Project Initialized

---
+++ MYSQL/tienda.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS TiendaDB;
+CREATE DATABASE TiendaDB;
+
+USE TiendaDB;
+
+CREATE TABLE Productos(
+	prod_id INT UNSIGNED NOT NULL,
+	prod_nombre VARCHAR(50) NOT NULL,
+	prod_descripcion TEXT,
+	prod_precio DECIMAL(12,4),
+	PRIMARY KEY(prod_id)
+);
