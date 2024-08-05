461372855c296722906df4ed539af192c946729b
GinoCas <gino.casentini.5@gmail.com>
2024-08-05 12:50:13 -0300

    Add producto controller

---
+++ API/Controllers/ProductoController.cs
@@ -0,0 +1,11 @@
+﻿using API.Controllers.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("/productos")]
+    public class ProductoController
+    {
+    }
+}
