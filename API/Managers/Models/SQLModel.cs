8d51458e685cd66d671e5eb63eea5173fcf05f23
GinoCas <gino.casentini.5@gmail.com>
2024-08-08 09:04:55 -0300

    Add mysql manager

---
+++ API/Managers/Models/SQLModel.cs
@@ -0,0 +1,12 @@
+﻿using API.Configuration;
+
+namespace API.Managers.Models
+{
+    public interface ISQLModel
+    {
+        public DataBaseConnection dbConnection { get; set; }
+        public void OpenConnection();
+        public void CloseConnection();
+        public void ExecuteQuery(string query, params object[] parameters);
+    }
+}
---
+++ API/Managers/MySQLManager.cs
@@ -0,0 +1,47 @@
+﻿using API.Configuration;
+using API.Managers.Models;
+using MySqlConnector;
+
+namespace API.Managers
+{
+    public class MySQLManager : ISQLModel
+    {
+        public DataBaseConnection dbConnection { get; set; }
+        private MySqlConnection mysqlConnection;
+
+        public MySQLManager(DataBaseConnection dbConnection)
+        {
+            this.dbConnection = dbConnection;
+            mysqlConnection = new MySqlConnection(dbConnection.connectionString);
+        }
+        public void OpenConnection()
+        {
+            if (mysqlConnection.State == System.Data.ConnectionState.Open){ return; }
+            mysqlConnection.Open();
+        }
+        public void CloseConnection() 
+        {
+            if (mysqlConnection.State == System.Data.ConnectionState.Closed) { return; }
+            mysqlConnection.Close();
+        }
+        public async void ExecuteQuery(string query, params object[] parameters)
+        {
+            OpenConnection();
+            using (var cmd = new MySqlCommand(query, mysqlConnection))
+            {
+                cmd.CommandType = System.Data.CommandType.Text;
+                using (var reader = cmd.ExecuteReaderAsync())
+                {
+                    while (await reader.Result.ReadAsync())
+                    {
+                        for (int i = 0; i < reader.Result.FieldCount; i++)
+                        {
+                            Console.WriteLine($"{reader.Result.GetName(i)}: {reader.Result.GetValue(i)}");
+                        }
+                    }
+                }
+            }
+            CloseConnection();
+        }
+    }
+}
