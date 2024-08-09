3386fcd273794c0b532554ca5a32952cc0bfa62a
GinoCas <gino.casentini.5@gmail.com>
2024-08-08 23:19:29 -0300

    Add application controller

---
+++ API/Controllers/ApplicationController.cs
@@ -0,0 +1,11 @@
+﻿using API.Configuration;
+using API.Managers;
+using API.Models;
+
+namespace API.Controllers
+{
+    public class ApplicationController
+    {
+        public static IDatabaseModel dbManager = new MySQLManager(new DataBaseConnection());
+    }
+}
