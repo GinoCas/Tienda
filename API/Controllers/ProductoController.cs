3de18eef17ad63cfb9c3d88a4ec30e3a22e683a7
GinoCas <gino.casentini.5@gmail.com>
2024-08-08 23:19:09 -0300

    Add product endpoints

--- API/Controllers/ProductoController.cs
+++ API/Controllers/ProductoController.cs
@@ -1,11 +1,37 @@
-﻿using API.Controllers.Models;
+﻿using API.Configuration;
+using API.Handlers;
+using API.Managers;
+using API.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
     [ApiController]
-    [Route("/productos")]
+    [Route("productos")]
     public class ProductoController
     {
+        private ProductoHandler handler = new ProductoHandler();
+        [HttpGet]
+        public async Task<ActionResult<List<ProductoModel>>> Get()
+        {
+            return await handler.GetProducts();
+        }
+        [HttpPost("create")]
+        public async Task Post([FromBody] ProductoModel product)
+        {
+            await handler.InsertProduct(product);
+        }
+        [HttpPut("update/{id}")]
+        public async Task Put(int id, [FromBody] ProductoModel product)
+        {
+            product.Id = id;
+            await handler.UpdateProduct(product);
+        }
+        [HttpDelete("delete/{id}")]
+        public async Task Delete(int id, [FromBody] ProductoModel product)
+        {
+            product.Id = id;
+            await handler.DeleteProduct(product);
+        }
     }
 }
