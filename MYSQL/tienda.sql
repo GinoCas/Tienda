bd7cc37ba6d898eaa834b2407ef097a5802ae3ee
GinoCas <gino.casentini.5@gmail.com>
2024-08-08 23:16:41 -0300

    Add user table

--- MYSQL/tienda.sql
+++ MYSQL/tienda.sql
@@ -11,8 +11,20 @@ CREATE TABLE Producto(
 	PRIMARY KEY(prod_id)
 );
 
+CREATE TABLE Usuario(
+	user_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	user_nombre VARCHAR(50) NOT NULL,
+	user_password VARCHAR(50) NOT NULL,
+	PRIMARY KEY(user_id)
+);
+
 INSERT INTO producto(prod_nombre, prod_descripcion, prod_precio)
 VALUES
 ("Banana", "Bananashi", 0),
 ("Manzana", "Es una manza viejo", 30),
 ("Producto", "no se es un producto", 60);
+
+INSERT INTO Usuario(user_nombre, user_password) 
+VALUES
+("Gino", "cas");
+#UPDATE PRODUCTO SET prod_nombre = 'Banana 2.0', prod_descripcion = 'Es una bananashi 2.0', prod_precio = 1 WHERE prod_id = 1
