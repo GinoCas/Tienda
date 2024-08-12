606cdbb22513dfeed9a9c95565dd10e423f1fcfd
GinoCas <gino.casentini.5@gmail.com>
2024-08-12 09:18:49 -0300

    Add user controller

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using API.Handlers;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("usuarios")]
+    public class UserController
+    {
+        private UserHandler handler = new UserHandler();
+        [HttpPost("login")]
+        public async Task<int> Login([FromBody] UserModel user)
+        {
+            UserModel? user_found = await handler.GetUser(user.Name);
+            if (user_found == null || user_found.Password != user.Password)
+            {
+                Console.WriteLine("Usuario o contraseña incorrecta.");
+                return 0;
+            }
+            Console.WriteLine("Bienvenido!");
+            return 1;
+        }
+        [HttpPost("register")]
+        public async Task<int> Register([FromBody] UserModel user)
+        {
+            UserModel? user_found = await handler.GetUser(user.Name);
+            if (user_found != null)
+            {
+                Console.WriteLine("Usuario ya existente.");
+                return 0;
+            }
+            Console.WriteLine("Usuario creado con exito.");
+            return await handler.PostUser(new UserModel(user.Name, user.Password));
+        }
+        /*
+        [HttpPost("register")]
+        public async Task Put(int id, [FromBody] ProductModel product)
+        {
+            product.Id = id;
+            await handler.UpdateProduct(product);
+        }
+        [HttpDelete("delete/{id}")]
+        public async Task Delete(int id, [FromBody] ProductModel product)
+        {
+            product.Id = id;
+            await handler.DeleteProduct(product);
+        }
+        */
+    }
+}
---
+++ API/Handlers/UserHandler.cs
@@ -0,0 +1,80 @@
+﻿using API.Controllers;
+using API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using System.Data;
+using System.Data.Common;
+
+namespace API.Handlers
+{
+    public class UserHandler
+    {
+        private List<UserModel> ConvertDataTable(DataTable dataTable)
+        {
+            var list = new List<UserModel>();
+            foreach (DataRow row in dataTable.Rows)
+            {
+                var user = new UserModel();
+                user.Id = (int)row["user_id"];
+                user.Name = row["user_name"].ToString();
+                user.Password = row["user_password"].ToString();
+                user.EntryDate = (DateTime)row["user_entrydate"];
+                list.Add(user);
+            }
+            return list;
+        }
+        public async Task<UserModel?> GetUser(string user)
+        {
+            UserModel user_found;
+            try
+            {
+                user_found = ConvertDataTable(await ApplicationController.dbManager.ExecuteQueryAsync(
+                "SELECT * FROM Users " +
+                $"WHERE user_name = @user_name", 
+                new DBParameter("@user_name", user)))[0];
+            }
+            catch
+            {
+                return null;
+            }
+            return user_found;
+        }
+        public async Task<int> PostUser(UserModel user)
+        {
+            return await ApplicationController.dbManager.ExecuteNonQueryAsync(
+                "INSERT INTO Users" +
+                "(user_name, user_password) " +
+                "VALUES" +
+                $"(@user_name, @user_password);", 
+                new DBParameter("@user_name", user.Name),
+                new DBParameter("@user_password", user.Password));
+        }
+        /*
+        public async Task<int> InsertProduct(ProductModel product)
+        {
+            return await ApplicationController.dbManager.ExecuteNonQueryAsync(
+                "INSERT INTO PRODUCTO" +
+                "(prod_nombre, prod_descripcion, prod_precio) " +
+                "VALUES" +
+                $"('{product.Nombre}', '{product.Descripcion}', {product.Precio});");
+        }
+        public async Task<int> UpdateProduct(ProductModel product)
+        {
+            return await ApplicationController.dbManager.ExecuteNonQueryAsync(
+                "UPDATE PRODUCTO " +
+                "SET " +
+                $"prod_nombre = '{product.Nombre}', " +
+                $"prod_descripcion = '{product.Descripcion}', " +
+                $"prod_precio = {product.Precio} " +
+                $"WHERE prod_id = {product.Id};");
+        }
+        public async Task<int> DeleteProduct(ProductModel product)
+        {
+            return await ApplicationController.dbManager.ExecuteNonQueryAsync(
+                "DELETE FROM PRODUCTO " +
+                $"WHERE prod_id = {product.Id};");
+        }*/
+    }
+}
