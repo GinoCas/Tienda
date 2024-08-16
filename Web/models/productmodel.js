bee3b3b67bf6f8396a9279e469cec625d8db31f2
GinoCas <gino.casentini.5@gmail.com>
2024-08-16 20:52:56 -0300

    front: Add product model

---
+++ Web/models/productmodel.js
@@ -0,0 +1,10 @@
+export class ProductModel{
+    constructor(id, name, code, description, price, stock){
+        this.id = id;
+        this.name = name;
+        this.code = code;
+        this.description = description;
+        this.price = price;
+        this.stock = stock;
+    }
+}
