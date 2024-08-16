b947f17533961119205d1f6b85dad7246d119b41
GinoCas <gino.casentini.5@gmail.com>
2024-08-16 20:53:50 -0300

    front: Add product controller

---
+++ Web/global/products.js
@@ -0,0 +1,20 @@
+import { GetHandler } from "../../../api/apicontroller.js";
+import { ProductModel } from "../../../models/productmodel.js";
+
+export async function GetProductList(){
+    try{
+        const data = await GetHandler('/productos');
+        return data.map(
+            product => new ProductModel(
+                product.id,
+                product.name,
+                product.code,
+                product.description,
+                product.price,
+                product.stock
+            )
+        );
+    }catch{
+        return [];
+    }
+}
