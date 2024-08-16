25a3195526a5f96e96edb76bd62dcb45e74f06d9
GinoCas <gino.casentini.5@gmail.com>
2024-08-16 20:55:02 -0300

    back: Add cors builder

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.AspNetCore.Cors.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
-var app = builder.Build();
-
+var app = builder.Build();  
+app.UseCors(builder => builder.WithOrigins("http://127.0.0.1:5500")
+                              .AllowAnyHeader()
+                              .AllowAnyMethod());
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
