99685e7212529ce15871e4280d218a98e43ffb68
GinoCas <gino.casentini.5@gmail.com>
2024-08-17 12:28:13 -0300

    front: Add usermodel

---
+++ Web/models/usermodel.js
@@ -0,0 +1,9 @@
+export class UserModel{
+    constructor(id, name, surname, email, password){
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+    }
+}
