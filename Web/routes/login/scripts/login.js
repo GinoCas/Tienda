49c70a1a2db06d7116c13d597cce62cc8948a9f1
GinoCas <gino.casentini.5@gmail.com>
2024-08-17 13:53:14 -0300

    front: Add login error

--- Web/routes/login/scripts/login.js
+++ Web/routes/login/scripts/login.js
@@ -1,9 +1,20 @@
 import { LoginUser } from "../../../global/usercontroller.js";
 
 const form = document.getElementById('login-form');
+const formError = document.getElementById('login-error');
 
 form.addEventListener('submit', async (event) => {
     event.preventDefault();
     const response = await LoginUser(new FormData(form));
-    console.log(response);
+    if(response.ok){
+        return window.location.href = '/routes/main/main.html';
+    }
+    const error_text = document.createElement('p');
+    formError.appendChild(error_text);
+    formError.style = 'padding: 8px;'
+
+    if(response.status == 401){
+        return error_text.textContent = 'Usuario o contraseña incorrecta.';
+    }
+    return error_text.textContent = "Error al conectarse con el servidor.";
 });
