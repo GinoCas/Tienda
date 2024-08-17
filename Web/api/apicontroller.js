5e9160a37ae2755fd1385a0ea33894832c009627
GinoCas <gino.casentini.5@gmail.com>
2024-08-17 12:26:47 -0300

    front: Add posthandler to apicontroller

--- Web/api/apicontroller.js
+++ Web/api/apicontroller.js
@@ -1,6 +1,14 @@
-const API_URL = 'http://localhost:5047';
+const API_URL = 'http://localhost:5047/';
 
 export async function GetHandler(route){
     const response = await fetch(API_URL + route);
     return response.json();
+}
+
+export async function PostHandler(route, data){
+    return await fetch(API_URL + route, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data),
+    });
 }
